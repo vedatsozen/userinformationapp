b7da1bc5b52acfe88b3c521b09e0b2674ee65a53
Vedat Sözen <vedatsozen@gmail.com>
2022-06-25 19:31:09 +0300

    Add files via upload

---
+++ members.js
@@ -0,0 +1,50 @@
+const members = [
+
+{
+id:1,
+name:"Vedat",
+surname:"Sözen",
+age:40,
+job:"Front End Developer",
+blood:"0 RH +",
+language:"Turkish",
+country:"Turkey",
+hobbies: ["Playing Basketball","Web Development","Internet Searching"] ,
+skills: ["Html","Css","Javascript"],
+shortcv: "Hello, My name is Vedat. I am from Turkey. I was born in 1982. I studied economics in Turkey. I decided to earn my life as a Front End Developer after 40 years old. I am good at html and css. I am improving my Javascript skills. And I am also learning React js too nowadays. See you then.",
+socialmedia: ["https://www.twitter.com/vedatsozen","https://www.github.com/vedatsozen","https://www.codepen.io/vedatsozen"],
+source: "profilfoto.jpg"
+},
+
+{
+    id:1,
+    name:"Brad",
+    surname:"Pitt",
+    age:50,
+    job:"Actor",
+    blood:"I do not know",
+    language:"English",
+    country:"United States",
+    hobbies: ["Acting","Hobby 2","Hobby 3"] ,
+    skills: ["Skill 1","Skill 2","Skill 3"],
+    shortcv: "Hello, My name is Brad Pitt. I am from USA. I played in so many hollywood movies.",
+    socialmedia: ["Twitter","Github","Codepen"],
+    source:"brad.jpg"
+    },
+
+    {
+        id:1,
+        name:"Hasan",
+        surname:"Hüseyin",
+        age:40,
+        job:"Front End Developer",
+        blood:"0 RH +",
+        language:"Turkish",
+        country:"Turkey",
+        hobbies: ["Playing Basketball","Web Development","Internet Searching"] ,
+        skills: ["Html","Css","Javascript"],
+        shortcv: "Hello, My name is Vedat. I am from Turkey. I was born in 1982. I studied economics in Turkey. I decided to earn my life as a Front End Developer after 40 years old. I am good at html and css. I am improving my Javascript skills. And I am also learning React js too nowadays. See you then.",
+        socialmedia: ["https://www.twitter.com/vedatsozen","https://www.github.com/vedatsozen","https://www.codepen.io/vedatsozen"]
+        }
+
+];
